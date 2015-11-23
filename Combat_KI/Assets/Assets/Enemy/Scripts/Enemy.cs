00c1102834e739d997cdba57c8e8a9950b0ee3b6
Thorsten Fuchs <thorsten2991@gmx.de>
2015-11-23 19:06:07 +0100

    Very basic enemy
    Enemy moves towards the player
    3 States (idle, search, attack)
    Can be killed (right now implemented with raycast)

---
+++ Combat_KI/Assets/Assets/Enemy/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public Material[] materials;
+    public int hp;
+
+    public GameObject player;
+    public float fov;
+
+    private EnemyState state;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        ChangeState(EnemyState.stateIDs.Idle);
+    }
+
+    void FixedUpdate()
+    {
+        state.action();
+    }
+
+    void OnHit(int dmg)
+    {
+        hp -= dmg;
+        if(hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void ChangeState(EnemyState.stateIDs _stateID)
+    {
+        if(this.state == null || this.state.getID() != _stateID)
+        {
+            switch(_stateID)
+            {
+                case EnemyState.stateIDs.Idle:
+                    state = new EnemyStateIdle(this);
+                    break;
+                case EnemyState.stateIDs.Search:
+                    state = new EnemyStateSearching(this);
+                    break;
+                case EnemyState.stateIDs.Attack:
+                    state = new EnemyStateAttack(this);
+                    break;
+                default:
+                    Debug.LogError("False enemy state provided");
+                    break;
+            }
+        }
+    }
+}
