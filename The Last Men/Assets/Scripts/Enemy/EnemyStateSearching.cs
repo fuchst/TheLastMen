601d17ab0ac7705cb9a49d74ee5dc098acc2e1bf
JingYi <jyw@live.de>
2015-11-25 14:22:33 +0100

    Combat imported in main Project

---
+++ The Last Men/Assets/Scripts/Enemy/EnemyStateSearching.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStateSearching : EnemyState {
+
+    const stateIDs id = stateIDs.Search;
+
+    public EnemyStateSearching(Enemy _enemy) 
+        : base(_enemy)
+    {
+        Debug.Log("Search state");
+        _enemy.GetComponent<Renderer>().material = _enemy.materials[1];
+    }
+
+    public override void action()
+    {
+        Vector3 playerPos = enemy.player.transform.position;
+        Vector3 dir = (playerPos - this.enemy.transform.position).normalized;
+
+        float angle = Vector3.Angle(dir, enemy.transform.forward);
+
+        if(angle < enemy.fov)
+        {
+            enemy.SendMessage("ChangeState", EnemyState.stateIDs.Attack);
+        }
+    }
+}
+
