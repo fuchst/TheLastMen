f20b60250b3185b5716d7a318ad6428cb879b8c7
Jing Yi Wang <jyw@live.de>
2016-02-02 15:05:22 +0100

    more fixes level 2 works

--- The Last Men/Assets/Scripts/Enemy/FlyingEnemy/PlaceFlyingEnemy.cs
+++ The Last Men/Assets/Scripts/Enemy/FlyingEnemy/PlaceFlyingEnemy.cs
@@ -3,18 +3,23 @@
 public class PlaceFlyingEnemy : MonoBehaviour {
 
     public static Transform flyingEnemyParent;
+    public static int maxFlyingEnemies = 25;
+    public static int currentFlyingEnemeis = 0;
     GameObject prefab;
 
-    void Awake()
-    {
-        prefab = Resources.Load("FlyingEnemy") as GameObject;
-    }
-
-	// Use this for initialization
+    //void Awake()
+    //{
+    //    prefab = Resources.Load("FlyingEnemy") as GameObject;
+    //}
+    
 	void Start ()
     {
-        Vector3 spawnpoint = transform.position + transform.up * 10.0f;
-        GameObject flyingEnemy = Instantiate(prefab, spawnpoint, transform.rotation) as GameObject;
-        flyingEnemy.transform.parent = flyingEnemyParent;
+        if (currentFlyingEnemeis < maxFlyingEnemies)
+        {
+            Vector3 spawnpoint = transform.position + transform.up * 10.0f;
+            GameObject flyingEnemy = Instantiate(Resources.Load("FlyingEnemy") as GameObject, spawnpoint, transform.rotation) as GameObject;
+            flyingEnemy.transform.parent = flyingEnemyParent;
+            currentFlyingEnemeis++;
+        }
 	}
 }
